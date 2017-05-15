525d6d6c9f1552ad4a50e97e7e7547645a5d6c29
igoshein@gmail.com <igoshein@gmail.com>
2017-05-15 18:29:17 +0300

    Create class BlogControllerTest

---
+++ tests/AppStore/Controller/BlogControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use AppBundle\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Functional test for the controllers defined inside BlogController.
+ *
+ * See https://symfony.com/doc/current/book/testing.html#functional-tests
+ *
+ * Execute the application tests using this command (requires PHPUnit to be installed):
+ *
+ *     $ cd your-symfony-project/
+ *     $ ./vendor/bin/phpunit
+ */
+class BlogControllerTest extends WebTestCase
+{
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/blog/');
+
+        $this->assertCount(
+            Post::NUM_ITEMS,
+            $crawler->filter('article.post'),
+            'The homepage displays the right number of posts.'
+        );
+    }
+
+}
