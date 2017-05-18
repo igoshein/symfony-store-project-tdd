5a83d49e8e3f1a3d1fccaa0a042584fcb845f4ca
igoshein@gmail.com <igoshein@gmail.com>
2017-05-18 18:43:47 +0300

    Create class BlogControllerTest

---
+++ tests/AppStore/Controller/Admin/BlogControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\AppBundle\Controller\Admin;
+
+use AppBundle\DataFixtures\FixturesTrait;
+use AppBundle\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Functional test for the controllers defined inside the BlogController used
+ * for managing the blog in the backend.
+ *
+ * See https://symfony.com/doc/current/book/testing.html#functional-tests
+ *
+ * Whenever you test resources protected by a firewall, consider using the
+ * technique explained in:
+ * https://symfony.com/doc/current/cookbook/testing/http_authentication.html
+ *
+ * Execute the application tests using this command (requires PHPUnit to be installed):
+ *
+ *     $ cd your-symfony-project/
+ *     $ ./vendor/bin/phpunit
+ */
+class BlogControllerTest extends WebTestCase
+{
+
+    use FixturesTrait;
+
+    /**
+     * @dataProvider getUrlsForRegularUsers
+     */
+    public function testAccessDeniedForRegularUsers($httpMethod, $url)
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'john_user',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+
+        $client->request($httpMethod, $url);
+        $this->assertSame(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
+    }
+
+}
