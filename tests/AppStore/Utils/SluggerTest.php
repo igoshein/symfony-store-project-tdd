30ef70f2c0d6fdafc27c9a726282093ac7e67340
igoshein@gmail.com <igoshein@gmail.com>
2017-06-18 19:14:54 +0300

    Create class SluggerTest

---
+++ tests/AppStore/Utils/SluggerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\AppBundle\Utils;
+
+use AppBundle\Utils\Slugger;
+
+/**
+ * Unit test for the application utils.
+ *
+ * See https://symfony.com/doc/current/book/testing.html#unit-tests
+ *
+ * Execute the application tests using this command (requires PHPUnit to be installed):
+ *
+ *     $ cd your-symfony-project/
+ *     $ ./vendor/bin/phpunit
+ */
+class SluggerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function getSlugs()
+    {
+        yield ['Lorem Ipsum', 'lorem-ipsum'];
+        yield ['  Lorem Ipsum  ', 'lorem-ipsum'];
+        yield [' lOrEm  iPsUm  ', 'lorem-ipsum'];
+        yield ['!Lorem Ipsum!', '!lorem-ipsum!'];
+        yield ['lorem-ipsum', 'lorem-ipsum'];
+        yield ['lorem 日本語 ipsum', 'lorem-日本語-ipsum'];
+        yield ['lorem русский язык ipsum', 'lorem-русский-язык-ipsum'];
+        yield ['lorem العَرَبِيَّة‎‎ ipsum', 'lorem-العَرَبِيَّة‎‎-ipsum'];
+    }
+
+}
