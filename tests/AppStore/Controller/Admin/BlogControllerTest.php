45a9bfe38fdf7643aec09b649d138e22e703251c
igoshein@gmail.com <igoshein@gmail.com>
2017-08-04 14:38:30 +0300

    Add files

---
+++ tests/AppStore/Controller/Admin/BlogControllerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\AppBundle\Controller\Admin;
+
+use AppBundle\DataFixtures\FixturesTrait;
+use AppBundle\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Functional test for the controllers defined inside the BlogController used
+ * for managing the blog in the backend.
+ *
+ * See https://symfony.com/doc/current/book/testing.html#functional-tests
+ *
+ * Whenever you test resources protected by a firewall, consider using the
+ * technique explained in:
+ * https://symfony.com/doc/current/cookbook/testing/http_authentication.html
+ *
+ * Execute the application tests using this command (requires PHPUnit to be installed):
+ *
+ *     $ cd your-symfony-project/
+ *     $ ./vendor/bin/phpunit
+ */
+class BlogControllerTest extends WebTestCase
+{
+
+    use FixturesTrait;
+
+    /**
+     * @dataProvider getUrlsForRegularUsers
+     */
+    public function testAccessDeniedForRegularUsers($httpMethod, $url)
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'john_user',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+
+        $client->request($httpMethod, $url);
+        $this->assertSame(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
+    }
+
+    public function getUrlsForRegularUsers()
+    {
+        yield ['GET', '/en/admin/post/'];
+        yield ['GET', '/en/admin/post/1'];
+        yield ['GET', '/en/admin/post/1/edit'];
+        yield ['POST', '/en/admin/post/1/delete'];
+    }
+
+    public function testAdminBackendHomePage()
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'jane_admin',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+
+        $crawler = $client->request('GET', '/en/admin/post/');
+        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+
+        $this->assertGreaterThanOrEqual(
+            1,
+            $crawler->filter('body#admin_post_index #main tbody tr')->count(),
+            'The backend homepage displays all the available posts.'
+        );
+    }
+
+}
